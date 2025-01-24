a2fac82d60aa7fed9411f9307657782184643f8c
Sebin Joseph <josephsebin012@gmail.com>
2025-01-24 14:09:29 +0530

    Add files via upload

---
+++ sebin.sh
@@ -0,0 +1,15 @@
+echo "enter the name"
+read name
+echo "enter the semester"
+read semester
+echo "enter the batch"
+read batch
+echo "enter the roll no"
+read roll no
+echo "enter the register number"
+read register number
+echo "name:"$name
+echo "semester:"$semester
+echo "batch:"$batch
+echo "roll no:"$roll no
+echo "register:"$register
